a03c337529e1f05336c921489a7dbfc395b0dea3
Eric Zhao <21zhaoe@protonmail.com>
2019-10-21 19:07:11 -0400

    Confreader: add regular Java starter

---
+++ confreader/starter-code/java/java/src/confreader/ConfReader.java
@@ -0,0 +1,14 @@
+package confreader;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConfReader {
+
+    public Map<String, Object> parse(File f) {
+        // Implementation here
+        Map<String, Object> parsedMap = new HashMap<>();
+        return parsedMap;
+    }
+}
