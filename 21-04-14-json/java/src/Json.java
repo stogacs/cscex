cae3f2ac9388f9737dbc449c1796c19c0076dfbd
Eric Zhao <21zhaoe@protonmail.com>
2021-04-13 22:48:04 -0400

    json: Add Java stub

---
+++ 21-04-14-json/java/src/Json.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class Json {
+
+    public static HashMap<String, Object> parse(String json) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    public static <T> T parseDeserialize(String json) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    /*
+     I TRIED WRITING TESTS BUT IT WAS TOO TEDIOUS SO I GAVE UP.
+     FEEL FREE TO USE PRINT STATEMENTS TO SEE IF IT'S WORKING.
+     */
+}
